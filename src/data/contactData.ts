070d66f0098fcb1482aa8a8e83de980b0d128ffb
Invitado <kevin.jordan@correounivalle.edu.co>
2026-02-24 11:44:51 -0500

    feat: add contact section base structure

---
+++ src/data/contactData.ts
@@ -0,0 +1,8 @@
+import { ContactData } from "@/types";
+
+export const contacData: ContactData[] = [
+    { name: "Github", user: "kevinJ1910", url: "https://github.com/kevinJ1910"},
+    { name: "Email", user: "kevin.jordan@correounivalle.edu.co",  url: "mailto:kevin.jordan@correounivalle.edu.co"},
+    { name: "LinkedIn", user: "kevin-jordan-alzate",  url: "https://www.linkedin.com/in/kevin-jordan-alzate-192b86366/"},
+    { name: "Instagram", user: "kevnjordn_lz", url: "https://www.instagram.com/kevnjordn_lz/"},
+]
--- src/types/index.ts
+++ src/types/index.ts
@@ -30,3 +30,9 @@ export interface Project {
       suggestedQuestions: { q: string; a: string }[]
     }
   }
+
+  export interface ContactData {
+    name: string
+    user: string
+    url: string
+  }
